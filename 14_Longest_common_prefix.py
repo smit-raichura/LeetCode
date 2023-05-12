7843e0938eaa72c786609310fd991b22b47caf75
Smit Raichura <97978110+smit-raichura@users.noreply.github.com>
2023-05-12 10:32:36 -0400

    Add files via upload

---
+++ 14_Longest_common_prefix.py
@@ -0,0 +1,9 @@
+def longestCommonPrefix(strs: List[str]) -> str:
+    prefix = ''
+    for i in range(len(strs[0])):
+        for s in strs:
+            # print('string : {0}     index : {1}     cahrAt : {2}'.format(s, i, s[i]))
+            if i == len(s) or s[i] != strs[0][i]:
+                return prefix
+        prefix += strs[0][i]
+    return prefix
